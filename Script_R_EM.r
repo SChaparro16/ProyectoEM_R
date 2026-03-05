ac377d1e906f35343da99a3d4d1675cd0652834b
SChaparro16 <schapalo1608@gmail.com>
2026-03-04 19:02:39 -0500

    Versión 1.0 (inicio)

--- Script_R_EM.r
+++ Script_R_EM.r
@@ -1,5 +1,25 @@
 # ---- Dependencias ----
+
+# Lectura de archivos
+library(readr)
+library(readxl)
+# Procesamiento de datos
+library(dplyr)
+library(naniar)
+library(tidyr)
+library(fastDummies)
+library(missForest)
+# Graficar datos
+library(ggplot2)
+# Tablas
+library(kableExtra)
+# Encuestas
+library(srvyr)
+
 # ---- Datos ----
+
+setwd("/home/FED_SCHA/Documentos/ProyectoEM_R/Datos/")
+
 # ---- Anotaciones ----
 # ---- Procesamiento ----
 # ---- Estadísticas ----
